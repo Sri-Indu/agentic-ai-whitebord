88917837a4d16e6ae519c09b6ea388089f384739
Sri-Indu <sriindugaddam@gmail.com>
2026-08-28 21:56:38 +0530

    fix sidebar client component

--- components/custom/dashboard/AppSidebar.tsx
+++ components/custom/dashboard/AppSidebar.tsx
@@ -1,31 +1,56 @@
+"use client"
+
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenuButton,
 } from "@/components/ui/sidebar"
 import Image from "next/image"
+import { Button } from "@/components/ui/button"
+import { LayoutGrid } from "lucide-react"
+import { usePathname } from "next/navigation"
 
 export function AppSidebar() {
+  const path = usePathname()
+
   return (
     <Sidebar>
-      <SidebarHeader>
+      <SidebarHeader className="p-4">
         <div className="flex items-center gap-2">
-            
+          <Image
+            src="/logo.svg"
+            alt="Logo"
+            width={40}
+            height={40}
+          />
+
+          <h2 className="text-xl font-bold">
+            WhizBoard
+          </h2>
         </div>
-        <Image
-          src="/logo.svg"
-          alt="Logo"
-          width={40}
-          height={40}
-        />
-        <h2 className="text-xl font-bold">WhizBoard</h2>
       </SidebarHeader>
 
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <Button className="w-full">
+            + Create New Board
+          </Button>
+        </SidebarGroup>
+
+        <SidebarGroup>
+          <SidebarGroupLabel>
+            My Boards
+          </SidebarGroupLabel>
+
+          <SidebarMenuButton className="p-5" isActive={true}>
+            <LayoutGrid />
+            <span>All Files</span>
+          </SidebarMenuButton>
+        </SidebarGroup>
       </SidebarContent>
 
       <SidebarFooter />
