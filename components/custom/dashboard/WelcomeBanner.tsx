e1a0cc7a47ca58d13445864c65b49d8369621443
Sri-Indu <sriindugaddam@gmail.com>
2026-08-31 22:12:46 +0530

    Add dashboard page

---
+++ components/custom/dashboard/WelcomeBanner.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { useUser } from "@clerk/nextjs"
+import { Sparkle } from "lucide-react"
+import React from "react"
+import { Button } from "@/components/ui/button"
+
+function WelcomeBanner() {
+  const { user } = useUser()
+
+  return (
+    <div>
+      <div className="p-10 border rounded-xl bg-gradient-to-r from-blue-200 to-purple-200">
+        <h2 className="text-2xl font-bold">
+          Welcome Back, {user?.fullName}
+        </h2>
+
+        <p className="mt-2">Bring your Ideas to Life on Infinite canvas</p>
+
+        <div className="flex items-center gap-2 mt-5">
+          <Button>+ Create New Board</Button>
+          <Button variant="outline">
+            <Sparkle /> AI Helper
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WelcomeBanner
